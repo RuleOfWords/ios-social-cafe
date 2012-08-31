b735fb511704f3bc21689c1298cf860bc48c495c
Christine Abernathy <ca.abernathy@fb.com>
2012-08-31 08:50:47 -0300

    Bug fix for parsing URL.

--- SocialCafe/SocialCafe/AppDelegate.m
+++ SocialCafe/SocialCafe/AppDelegate.m
@@ -36,9 +36,11 @@
     NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
     for (NSString *pair in pairs) {
         NSArray *kv = [pair componentsSeparatedByString:@"="];
-        NSString *val = [[kv objectAtIndex:1]
+        if ([kv count] > 1) {
+            NSString *val = [[kv objectAtIndex:1]
                          stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-        [params setObject:val forKey:[kv objectAtIndex:0]];
+            [params setObject:val forKey:[kv objectAtIndex:0]];
+        }
     }
     return params;
 }
